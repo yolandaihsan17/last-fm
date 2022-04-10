612753898d71635e0364424058806a5e1f2d93f5
yolanda <yolanda@landx.id>
2022-04-10 10:55:40 +0700

    add a little test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Footer from './components/footer'
 
-test('renders learn react link', () => {
+test('rendered app component without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+});
+
+test('rendered Author in footer', () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/Yolanda Ihsan/i);
   expect(linkElement).toBeInTheDocument();
 });
